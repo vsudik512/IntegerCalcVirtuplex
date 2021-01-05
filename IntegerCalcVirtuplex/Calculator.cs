a390e88b8ba16099b826464b586408881910f52a
vsudik <vsudik@werefactoritUS.onmicrosoft.com>
2021-01-05 20:20:35 +0100

    Initial Commit

---
+++ IntegerCalcVirtuplex/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IntegerCalcVirtuplex
+{
+    public static class Calculator
+    {
+        private static  readonly Regex sWhitespace = new Regex(@"\s+");
+        private static DataTable dataTable = new DataTable();
+
+        private static char[] validCharacters = new char[14] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '/', '*' };
+
+        public static async Task<string> CalculateFromString(string expression)
+        {
+            expression = sWhitespace.Replace(expression, "");
+
+            //Find out if the expression has got any invalid characters and store them in string 
+            string invalidCharacters = string.Join("", expression.Split(validCharacters));
+
+            if (invalidCharacters.Length > 0)
+                return string.Format("Error - Invalid characters: {0}", invalidCharacters);
+
+            int result = Convert.ToInt32(Math.Floor(Convert.ToDouble(dataTable.Compute(expression, null).ToString())));
+            return result.ToString();
+        }
+
+        public static async Task CalculateFromFile(string inputPath, string outputPath)
+        {
+            string[] lines = await File.ReadAllLinesAsync(inputPath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(lines[i]))
+                {
+                    lines[i] = "0";
+                    continue;
+                }
+                lines[i] = await CalculateFromString(lines[i]);
+            }
+
+            await File.WriteAllLinesAsync(outputPath, lines);
+        }
+    }
+}
---
+++ IntegerCalcVirtuplex/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IntegerCalcVirtuplex
+{
+    public partial class MainWindow : Window
+    {
+        private string expressionString;
+        private Label calculationLabel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculationLabel = this.FindName("CalculationLabel") as Label;
+        }
+
+        private void buttonDefault_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            expressionString += btn.Content;
+
+            UpdateLabel();
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(expressionString))
+                return;
+
+            expressionString = expressionString.Remove(expressionString.Length - 1);
+            UpdateLabel();
+        }
+
+        private void buttonEquals_Click(object sender, RoutedEventArgs e)
+        {
+            expressionString = Calculator.CalculateFromString(expressionString).Result;
+            UpdateLabel();
+        }
+
+        private async void buttonImport_Click(object sender, RoutedEventArgs e)
+        {
+
+            string inputFilepath = RequestOpenFileDialog();
+            string outputFilepath = RequestSaveFileDialog();
+
+            if (string.IsNullOrEmpty(inputFilepath) && string.IsNullOrEmpty(outputFilepath))
+            {
+                if (inputFilepath == outputFilepath)
+                {
+                    MessageBox.Show("Input and output should be two different files");
+                    return;
+                }
+
+                var btn = sender as Button;
+                btn.IsEnabled = false;
+
+                var progressBar = this.FindName("progressBarImport") as ProgressBar;
+                progressBar.IsIndeterminate = true;
+
+                await Task.Run(() => Calculator.CalculateFromFile(inputFilepath, outputFilepath));
+
+                btn.IsEnabled = true;
+                progressBar.IsIndeterminate = false;
+                MessageBox.Show("Calculation from file completed");
+            }
+        }
+
+        private string RequestOpenFileDialog()
+        {
+            OpenFileDialog inputFileDialog = new OpenFileDialog();
+            inputFileDialog.Title = "Input File";
+            inputFileDialog.Filter = "Text Files | *.txt";
+            inputFileDialog.DefaultExt = "txt";
+            if (inputFileDialog.ShowDialog() == true)
+            {
+                return inputFileDialog.FileName;
+            }
+            else
+                return string.Empty;
+        }
+
+        private string RequestSaveFileDialog()
+        {
+            SaveFileDialog outputFileDialog = new SaveFileDialog();
+            outputFileDialog.Title = "Output File";
+            outputFileDialog.Filter = "Text Files | *.txt";
+            outputFileDialog.DefaultExt = "txt";
+            if (outputFileDialog.ShowDialog() == true)
+            {
+                return outputFileDialog.FileName;
+            }
+            else
+                return string.Empty;
+        }
+
+        private void UpdateLabel()
+        {
+            calculationLabel.Content = expressionString;
+        }
+    }
+}
